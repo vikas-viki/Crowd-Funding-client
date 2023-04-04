ea11ef90445006255d15d5fd6ece2df26fa008ad
Vikas Kotary <viki57394@GMAIL.COM>
2023-04-04 22:13:56 +0530

    Basic done

---
+++ src/Context/index.jsx
@@ -0,0 +1,538 @@
+import React, { useContext, createContext, useState } from "react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "target",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "deadline",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "image",
+        type: "string",
+      },
+    ],
+    name: "CampaignCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_target",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_image",
+        type: "string",
+      },
+    ],
+    name: "createCampaign",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "_agree",
+        type: "bool",
+      },
+    ],
+    name: "donateToCampaign",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "donator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "agreed",
+        type: "bool",
+      },
+    ],
+    name: "DonationMade",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "refund",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "donator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "RefundIssued",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "withdrawFunds",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+    ],
+    name: "withdrawn",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "campaigns",
+    outputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "target",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "AmountCollected",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "image",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getCampaigns",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "title",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "description",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "target",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "deadline",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "AmountCollected",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "image",
+            type: "string",
+          },
+          {
+            internalType: "address[]",
+            name: "donators",
+            type: "address[]",
+          },
+          {
+            internalType: "uint256[]",
+            name: "donations",
+            type: "uint256[]",
+          },
+          {
+            internalType: "bool[]",
+            name: "agree",
+            type: "bool[]",
+          },
+        ],
+        internalType: "struct CrowdFunding.Campaign[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "getDonators",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numberOfCampaigns",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const Caddress = "0xDa106788f889dA588203a54B0B6356A291512255";
+
+export const StateContextProvider = ({ children }) => {
+  const [address, setAddress] = useState("");
+  const [contract, setContract] = useState({});
+
+  const connect = async () => {
+    // Check if the browser has MetaMask installed
+    if (typeof window.ethereum === "undefined") {
+      alert("Please install MetaMask first.");
+      return;
+    }
+
+    // Request access to the user's MetaMask account - directly.
+    // const provider = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    await provider.send("eth_requestAccounts");
+
+    const signer = await provider.getSigner();
+
+    setAddress(signer.address);
+
+    setContract(new ethers.Contract(Caddress, ABI, signer));
+  };
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await contract.createCampaign(
+        address, // owner
+        form.title, // title
+        form.description, // description
+        form.target,
+        new Date(form.deadline).getTime(), // deadline,
+        form.image
+      );
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+// [ todo]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const getCampaigns = async (specific = null) => {
+    const campaigns = await contract.getCampaigns();
+
+    const parsedCampaings = campaigns
+      .map((campaign, i) => {
+        const target = campaign.target
+          ? ethers.formatEther(campaign.target)
+          : null;
+        const amountCollected = campaign.AmountCollected
+          ? ethers.formatEther(campaign.AmountCollected)
+          : null;
+
+        if (specific && !campaign.title.includes(specific)) {
+          return null;
+        }
+
+        return {
+          owner: campaign.owner,
+          title: campaign.title,
+          description: campaign.description,
+          target: target,
+          deadline: Number(campaign.deadline),
+          amountCollected: amountCollected,
+          image: campaign.image,
+          pId: i,
+        };
+      })
+      .filter((campaign) =>
+        specific ? campaign.title.includes(specific) : true
+      );
+
+    return parsedCampaings;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+
+    return filteredCampaigns;
+  };
+
+  const donate = async (pId, amount) => {
+    console.log(pId, amount);
+    var data;
+    try {
+      // Convert the amount from Ether to Wei
+      const amountWei = ethers.parseEther(amount);
+
+      data = await contract.donateToCampaign(pId, true, {
+        value: amountWei,
+      });
+    } catch (error) {
+      alert(error.message);
+      return;
+    }
+
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.getDonators(pId);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      const weiToEth = ethers.formatEther(String(donations[1][i]));
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: weiToEth,
+      });
+    }
+    console.log(parsedDonations);
+
+    return parsedDonations;
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { StateContextProvider } from "./Context";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import "./index.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+  <Router>
+    <StateContextProvider>
       <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+    </StateContextProvider>
+  </Router>
 );
